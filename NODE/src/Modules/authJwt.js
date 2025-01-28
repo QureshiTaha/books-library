9395242068d9f72f59c50c6f2ea88196af972c3d
Taha Qureshi <taha.qureshi@profound.works>
2025-01-28 20:27:35 +0530

    Fixing Logic

--- NODE/src/Modules/authJwt.js
+++ NODE/src/Modules/authJwt.js
@@ -1,7 +1,7 @@
 const jwt = require('jsonwebtoken');
 
 const verifyToken = (req, res, next) => {
-  const token = req.headers.authorization?.split(' ')[1];
+  const token = req.headers.authorization ? req.headers.authorization.split(' ')[1] : null;
   if (!token) {
     return res.status(401).json({ message: 'Access denied. No token provided!' });
   }
